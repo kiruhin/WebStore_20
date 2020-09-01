0dc4554cfebda55a062291bdffd83fb44cd0f59b
Андрей Брижак <andrewbrizhak@gmail.com>
2020-09-02 07:33:16 +0800

    add action methods on BookController

--- WebStore_20/Controllers/BooksController.cs
+++ WebStore_20/Controllers/BooksController.cs
@@ -19,5 +19,81 @@ namespace WebStore.Controllers
         {
             _bookService = bookService;
         }
+
+        public IActionResult Books()
+        {
+            return View(_bookService.GetAll());
+        }
+
+        public IActionResult BookActionResultDetails(int id)
+        {
+            //Получаем  по Id
+            var book = _bookService.GetById(id);
+
+            //Если такого не существует
+            if (book == null)
+                return NotFound(); // возвращаем результат 404 Not Found
+
+            //Иначе возвращаем 
+            return View(book);
+        }
+
+        /// <summary>
+        /// Добавление или редактирование 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        //[Route("edit/{id?}")]
+        public IActionResult Edit(int? id)
+        {
+            if (!id.HasValue)
+                return View(new BookViewModel());
+
+            var model = _bookService.GetById(id.Value);
+            if (model == null)
+                return NotFound();// возвращаем результат 404 Not Found
+
+            return View(model);
+        }
+
+        [HttpPost]
+        //[Route("edit/{id?}")]
+        public IActionResult Edit(BookViewModel model)
+        {
+            if (model.Id > 0) // если есть Id, то редактируем модель
+            {
+                var dbItem = _bookService.GetById(model.Id);
+
+                if (ReferenceEquals(dbItem, null))
+                    return NotFound();// возвращаем результат 404 Not Found
+
+                dbItem.Author = model.Author;
+                dbItem.Name = model.Name;
+                
+            }
+            else // иначе добавляем модель в список
+            {
+                _bookService.AddNew(model);
+            }
+            _bookService.Commit(); // станет актуальным позднее (когда добавим БД)
+
+            return RedirectToAction(nameof(Books));
+        }
+
+        [HttpPost]
+        //[Route("delete/{id?}")]
+        public IActionResult Delete(int id)
+        {
+            if (id > 0)
+            {
+                _bookService.Delete(id);
+
+            }
+
+            return RedirectToAction(nameof(Books));
+            //return View(Employees());
+
+        }
     }
 }
