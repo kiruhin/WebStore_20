640d560ce36a6dc7a0009d21d0b7def8126005e2
Андрей Брижак <andrewbrizhak@gmail.com>
2020-09-02 07:19:19 +0800

    add DI on BooksController

--- WebStore_20/Controllers/BooksController.cs
+++ WebStore_20/Controllers/BooksController.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Interfaces.Infrastructure;
+using WebStore.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Infrastructure.Interfaces;
+
 
 namespace WebStore.Controllers
 {
-    public class BooksController
+    [Route("books/[action]")]
+    public class BooksController:Controller
     {
+        private readonly IBookService _bookService;
 
+        public BooksController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
     }
 }
