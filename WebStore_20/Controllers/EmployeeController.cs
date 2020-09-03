2f3de36ed0f75091b704c6f6e8138d4c8ac76348
ilya <illf.yar@googlemail.com>
2020-09-03 13:16:16 +0300

    Домашнее задание 3 лекции

--- WebStore_20/Controllers/EmployeeController.cs
+++ WebStore_20/Controllers/EmployeeController.cs
@@ -81,5 +81,10 @@ namespace WebStore.Controllers
 
             return RedirectToAction(nameof(Employees));
         }
+        public IActionResult Delete(int id)
+        {
+            _employeesService.Delete(id);
+            return RedirectToAction(nameof(Employees));
+        }
     }
 }
