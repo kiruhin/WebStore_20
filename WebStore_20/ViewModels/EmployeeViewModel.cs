30a7a7574c9af3f1cb575a4e4d6fcbe94b4886e8
Андрей Брижак <andrewbrizhak@gmail.com>
2020-09-05 16:46:14 +0800

    add validation on EmployeeViewModel

--- WebStore_20/ViewModels/EmployeeViewModel.cs
+++ WebStore_20/ViewModels/EmployeeViewModel.cs
@@ -1,12 +1,28 @@
-﻿namespace WebStore.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebStore.ViewModels
 {
     public class EmployeeViewModel
     {
         public int Id { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Reruired Name")]
+        [Display(Name = "Имя")]
+        [StringLength(maximumLength: 200, MinimumLength = 2, ErrorMessage = "Min 2 and Max 200 letters")]
         public string FirstName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Reruired SurName")]
+        [Display(Name = "Фамилия")]
         public string SurName { get; set; }
+
+        [Display(Name = "Отчество")]
         public string Patronymic { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Reruired Age")]
+        [Display(Name = "Возраст")]
         public int Age { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Reruired Position")]
+        [Display(Name = "Должность")]
         public string Position { get; set; }
     }
 
