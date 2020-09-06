513c2c36b708240b080e1a14ce96589c0751be71
Андрей Брижак <andrewbrizhak@gmail.com>
2020-09-06 13:08:38 +0800

    add class Product, add class ProductFilter, add InMemoryProductService

---
+++ WebStore.Domain/ProductFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebStore.Domain
+{
+    /// <summary>
+    /// Filter for Products
+    /// </summary>
+    public class ProductFilter
+    {
+        public int? CategoryId { get; set; }
+        public int? BrandId { get; set; }
+    }
+}
