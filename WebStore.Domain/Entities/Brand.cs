b1c9eac3998e9dc6b4fed38ddcc0615db7d83482
Андрей Брижак <andrewbrizhak@gmail.com>
2020-09-09 15:25:03 +0800

    add EntityFrameworkCore to WebStore.Domain and WebStore_5, add Annotation to WebStoreDomain

--- WebStore.Domain/Entities/Brand.cs
+++ WebStore.Domain/Entities/Brand.cs
@@ -3,6 +3,9 @@ using WebStore.Domain.Entities.Base.Interface;
 
 namespace WebStore.Domain.Entities
 {
+    /// <summary>
+    /// Brand of Products
+    /// </summary>
     public class Brand : NamedEntity, IOrderedEntity
     {
         public int Order { get; set; }
